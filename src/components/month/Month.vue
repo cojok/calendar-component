45ff7c6c5fe61d09be8677798bc0005956953396
flavius cojocariu <flavius.cojocariu@travador.com>
2018-10-30 19:52:19 +0100

    visual changes for month calendar

--- src/components/month/Month.vue
+++ src/components/month/Month.vue
@@ -1,3 +1,3 @@
-<!--<style src="./style.scss" lang="scss"></style>-->
+<style src="./style.scss" lang="scss"></style>
 <script src="./script.js"></script>
 <template src="./template.html"></template>
